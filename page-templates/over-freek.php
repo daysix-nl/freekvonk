13d2b0144b27ba8eedd719054617869fa8dcd46e
svennijhuis <sanijhuis@live.nl>
2023-09-23 17:01:00 +0200

    slider over freek nog niet af

--- page-templates/over-freek.php
+++ page-templates/over-freek.php
@@ -7,9 +7,23 @@
  get_header(); ?>
  
 <main class="pt-[150px]">
-    <section>
-        <div class="container">
-            <h1 class="text-55 leading-48 md:text-80 md:leading-66 lg:text-80 lg:leading-78 xl:text-80 xl:leading-78 font-tanker text-black max-w-[600px] mx-auto text-center"><?php the_title();?></h1>
+    <section class="container">
+        <div class="swiper mySwiper-navigatie">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide flex justify-center items-center">
+                    <a href="">Slide 1</a>
+                </div>
+                <div class="swiper-slide flex justify-center items-center">Slide 2</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 3</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 4</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 5</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 6</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 7</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 8</div>
+                <div class="swiper-slide flex justify-center items-center">Slide 9</div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
         </div>
     </section>   
 </main>
