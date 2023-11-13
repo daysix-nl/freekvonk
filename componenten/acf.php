5bc0f3fff4847813fd86aa775a6583a74ef3b892
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-11-13 09:06:20 +0100

    wip

--- componenten/acf.php
+++ componenten/acf.php
@@ -55,3 +55,7 @@
 <div class="drop-shadow-[0px_0px_15px_#8CC63F]"></div>
 
 <div class="pb-[30px] md:pb-[50px] lg:pb-[80px]"></div>
+
+<div class="justify-start"></div>
+<div class="justify-center"></div>
+<div class="justify-end"></div>
