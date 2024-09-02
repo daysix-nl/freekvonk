6afcade5ad61613e980256fd7649abab1790228b
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-09-02 15:47:03 +0200

    wip

--- blocks/acf-audio/acf-audio.php
+++ blocks/acf-audio/acf-audio.php
@@ -7,7 +7,7 @@ else: ?>
 <section id="<?php echo get_field('section_id');?>" class="<?php echo get_field('achtergrond');?> <?php echo get_field('padding_top');?> <?php echo get_field('padding_bottom');?> relative">
     <div class="container relative z-[40]">
         <div class="max-w-[390px] md:max-w-[603px] lg:max-w-[912px] mx-auto z-[40]">
-            <audio controls class="w-full">
+            <audio controls class="w-full min-h-[56px] bg-[#f1f3f4] rounded-full">
             <!-- <source src="horse.ogg" type="audio/ogg"> -->
             <source src="<?php echo get_field('mp3');?>" type="audio/mpeg">
             Je browser ondersteunt het audio-element niet.
