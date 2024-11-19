a04f8850a5ea338a8392a6e6c0cc150b1a311e6f
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-11-19 11:40:41 +0100

    Update header-black-notitle.php

--- header-black-notitle.php
+++ header-black-notitle.php
@@ -21,13 +21,7 @@
 </head>
 <body <?php body_class( 'page-block' ); ?>>
 
-<!-- Google Tag Manager -->
-<noscript><iframe loading="lazy" data-lazy="1" src="about:blank" class="tf_iframe_lazy" data-tf-src="//www.googletagmanager.com/ns.html?id=GTM-M73FKS" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-M73FKS');</script>
+
 
 
 <header class="fixed z-[999] bg-[#000]">
